3021d4fccb9c11f212eb380d9a87c54aeee38319
Chuong Le <chuong@poeticsystems.com>
2016-04-04 18:04:20 -0500

    add condition to make sure file/image exist before trying to load File entity.

--- modules/clutch/src/ParagraphBuilder.php
+++ modules/clutch/src/ParagraphBuilder.php
@@ -40,7 +40,7 @@ class ParagraphBuilder extends ClutchBuilder{
     $field_language = $field_definition->language()->getId();
     $field_value = $paragraph->get($field_name)->getValue();
     $field_type = $field_definition->getType();
-    if($field_type == 'image' || $field_type == 'file') {
+    if(($field_type == 'image' && !empty($field_value)) || ($field_type == 'file' && !empty($field_value))) {
       $file = File::load($field_value[0]['target_id']);
       $url = file_create_url($file->get('uri')->value);
       $field_value[0]['url'] = $url;
