53bfa4305d68af0919ef0bcf6cfaac62a6fe2ab9
Chuong Le <chuong@poeticsystems.com>
2016-05-11 12:31:39 -0500

    Update PageBuilder to create page using page manager/

--- modules/clutch/src/PageBuilder.php
+++ modules/clutch/src/PageBuilder.php
@@ -8,8 +8,9 @@
 namespace Drupal\clutch;
 
 use Symfony\Component\Yaml\Yaml;
-use Symfony\Component\DomCrawler\Crawler;
-use Drupal\paragraphs\Entity\Paragraph;
+use Drupal\page_manager\Entity\Page;
+use Drupal\page_manager\Entity\PageVariant;
+use Drupal\block_content\Entity\BlockContent;
 
 /**
  * Class PageBuilder.
@@ -20,7 +21,7 @@ use Drupal\paragraphs\Entity\Paragraph;
 class PageBuilder {
 
   /**
-   * Create pages
+   * Build Pages and associate Blocks with Pages
    *
    * @param $theme
    *   theme name
@@ -28,66 +29,102 @@ class PageBuilder {
    * @return
    *   TODO
    */
-  public function createPages($theme){
-    $page_infos = Yaml::parse(file_get_contents('themes/'.$theme.'/components.yml'));
-    foreach($page_infos as $page_title => $page_info){
-      $this->createPage($page_title, $page_info);
+  public function build($theme){
+    $page_infos = Yaml::parse(file_get_contents('themes/'.$theme.'/blocks.yml'));
+    $page_variants = $this->createPages($page_infos);
+    $block_uuids = $this->getBlocks();
+
+    foreach($page_infos as $page => $blocks) {
+      if(!empty($blocks)) {
+        $this->addBlockstoPage($blocks, $page_variants[$page], $block_uuids);
+      }
     }
+    drupal_flush_all_caches();
+  }
+
+  /**
+   * Create pages
+   *
+   * @param $page_infos
+   *   array of pages and blocks associated with pages
+   *
+   * @return $page_variants
+   *   array of page variants
+   */
+  public function createPages($page_infos) {
+    $pages = array_keys($page_infos);
+    $page_variants = array();
+    foreach($pages as $page) {
+      $page_variants[$page] = $this->createPage($page);
+    }
+    return $page_variants;
+  }
+
+  /**
+   * Get array of block uuids
+   *
+   * @return $block_uuids
+   *   array of block uuids. key is block bundle. value is block uuid
+   */
+  public function getBlocks() {
+    $block_ids = \Drupal::entityQuery('block_content')->execute();
+    $blocks = BlockContent::loadMultiple($block_ids);
+    $block_uuids = array();
+    foreach($blocks as $block) {
+      $block_uuids[$block->bundle()] = $block->uuid();
+    }
+    return $block_uuids;
   }
 
   /**
    * Create page
    *
-   * @param $page_title, $components
+   * @param $page_title
    *   page title
-   *   associated components
    *
    * @return
-   *   page entity
+   *   page variant entity
    */
-  public function createPage($page_title, $components) {
-    $associated_component_ids = array();
-    foreach($components['components'] as $component) {
-      array_push($associated_component_ids, $this->createAssociatedComponent($component));
-    }
-    $page_name = ucwords(str_replace('-', ' ', $page_title));
-    $page = entity_create('custom_page', array(
+  public function createPage($page_title) {
+    $page = Page::create(array(
       'id' => $page_title,
-      'name' => $page_name,
-      'associated_components' => $associated_component_ids,
+      'label' => ucwords(str_replace('_', ' ', $page_title)),
+      'path' => '/' . str_replace('_', '-', $page_title),
     ));
     $page->save();
+
+    $variant = PageVariant::create(array(
+      'id' => $page_title,
+      'label' => ucwords(str_replace('_', ' ', $page_title)),
+      'variant' => 'block_display',
+      'page' => $page_title,
+    ));
+    $variant->save();
+    return $variant;
   }
-
+  
   /**
-   * Create associated components for page
+   * Add Block Instances to Page Variant using Page manager mechanism 
    *
-   * @param $component
-   *   component string with component type and region
+   * @param $block_infos, $page_variant, $block_uuids
+   *   array of blocks associate to page
+   *   variant entity of page
+   *   array of block uuids to generate plugin id
    *
-   * @return
-   *   array of target_id, target_revision_id of component to associate to page
    */
-  public function createAssociatedComponent($component) {
-    $component_info_array = explode('|', $component);
-    $component_type = $component_info_array[0];
-    $component_position = $component_info_array[1];
-    $component_id = \Drupal::entityQuery('component')->condition('type',$component_type)->execute();
-    $paragraph = Paragraph::create([
-      'type' => 'associated_component',
-      'component' => [
-        'target_id' => key($component_id),
-      ],
-      'region' => [
-        'value' => !empty($component_position) ? $component_position:'content',
-        'format' => 'string',
-      ],
-    ]);
-
-    $paragraph->save();
-    return [
-      'target_id' => $paragraph->id(),
-      'target_revision_id' => $paragraph->getRevisionId(),
-    ];
+  public function addBlockstoPage($block_infos, $page_variant, $block_uuids) {
+    $blockManager = \Drupal::service('plugin.manager.block');
+    foreach($block_infos as $block) {
+      $block_uuid = $block_uuids[$block];
+      $block_plugin_id = "block_content:$block_uuid";
+      
+      $block_instance = $blockManager->createInstance($block_plugin_id);
+      $block_instance_configuration = $block_instance->getConfiguration();
+      // use 'top' as region for now
+      $block_instance_configuration['region'] = 'top';
+      $variant_plugin = $page_variant->getVariantPlugin();
+      $variant_plugin->addBlock($block_instance_configuration);
+    }
+    $page_variant->save();
   }
 }
